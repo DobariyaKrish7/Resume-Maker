a17d089ccb500b02ba5ccb9f81c703398ef02fad
DobariyaKrish7 <krishdobariya251@gmail.com>
2025-06-28 11:55:26 +0530

    backend done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,6 @@
-// import express from 'express';
-// import { registerUser } from '../controllers/userController.js';
-
-// const userRouter = express.Router();
-
-// userRouter.post('/register', registerUser);
-// userRouter.post('/login', loginUser);
-// userRouter.get('/profile', protect, getUserProfile);
-
-// //protected routes
 
 
-// export default userRouter;
+//done by dks
 
 import express from 'express';
 import {
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,4 @@
-// import express from 'express'
-// import cors from 'cors'
-// import 'dotenv/config'
-// import  {connectDB}  from './config/db.js'
 
-
-
-// const app = express()
-// const PORT = 3000
-
-// app.use(cors())
-
-// //connect to the database
-// connectDB();
-
-// // Middleware to parse JSON requests
-// app.use(express.json())
-
-
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the backend server!')
-// })
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`)
-// })
-// app.get('/api', (req, res) => {
-//   res.json({ message: 'Hello from the API!' })
-// })
 
 
 import express from 'express';
