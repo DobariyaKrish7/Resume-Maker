157eee137b9a81aa1d1e6dae1d252e37b2eb48a1
DobariyaKrish7 <krishdobariya251@gmail.com>
2025-07-10 15:14:07 +0530

    @

--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -27,24 +27,24 @@
 // }
 
 // src/utils/apiPath.js
-export const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
 
 export const API_PATHS = {
   AUTH: {
-    REGISTER: "/api/auth/register",
-    LOGIN: "/api/auth/login",
-    GET_PROFILE: "/api/auth/profile", // fixed typo from "GET_PROFLIE"
+    REGISTER: "/auth/register",
+    LOGIN: "/auth/login",
+    GET_PROFILE: "/auth/profile",
   },
   RESUME: {
-    CREATE: "/api/resume",
-    GET_ALL: "/api/resume",
-    GET_BY_ID: (id) => `/api/resume/${id}`,
-    UPDATE: (id) => `/api/resume/${id}`,
-    DELETE: (id) => `/api/resume/${id}`,
-    UPLOAD_IMAGES: (id) => `/api/resume/${id}/upload-images`,
+    CREATE: "/resume",
+    GET_ALL: "/resume",
+    GET_BY_ID: (id) => `/resume/${id}`,
+    UPDATE: (id) => `/resume/${id}`,
+    DELETE: (id) => `/resume/${id}`,
+    UPLOAD_IMAGES: (id) => `/resume/${id}/upload-images`,
   },
   IMAGE: {
-    UPLOAD_IMAGE: "/api/auth/upload-image",
+    UPLOAD_IMAGE: "/auth/upload-image",
   },
 };
